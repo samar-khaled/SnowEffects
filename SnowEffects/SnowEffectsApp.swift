073bb1ce29fb01a937a7a9a73a5658099885dcce
Samar Khaled <samar.khaled@gmail.com>
2025-01-04 14:44:47 +0200

    Initial Commit

---
+++ SnowEffects/SnowEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowEffectsApp.swift
+//  SnowEffects
+//
+//  Created by Samar Khaled on 04/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SnowEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
